a1891215f781eb2f3ee3c246edbde6b60be311d6
Klondike89 <brianjmurphy0830@gmail.com>
2022-01-13 21:12:38 -0600

    build(feature): background image and button field

--- Py_Clock.py
+++ Py_Clock.py
@@ -9,9 +9,13 @@ def get_time():
     clock.config(text=display_time)
     clock.after(20, get_time)
     
+C = Canvas(window, bg="blue",height="880",width="1700")
+C.pack(fill = "both", expand = True)
+filename = PhotoImage(file = r"C:\Users\brian\Desktop\PYTHON\Py_Clock\US-Time-Zone-Map.png")
+C.create_image(0, 0, image = filename, anchor = "nw")
 
-clock = Label(window, fg="white", bg="black", font = "Hurmit_NF 50", width=30, height=5)
-clock.pack()
+clock = Label(window, fg="white", bg="black", font = "Hurmit_NF 50", width=30, height=2)
+clock.place(x=100,y=500)
 
 get_time()
 
